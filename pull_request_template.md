ad1f94eccb2270d88dcfe2196fee2f21353fd73e
Senol Atac <senol.atac.1@gmail.com>
2021-11-29 13:59:52 +0300

    initial

---
+++ pull_request_template.md
@@ -0,0 +1,20 @@
+## Description
+
+> Description here...
+
+## Checklists
+
+### Development and testing
+
+- [ ] Related unit test have been implemented/updated
+- [ ] Application changes have been tested locally
+- [ ] Related documents have been updated
+
+### Security
+
+- [ ] Security impact of the change has been considered
+- [ ] Code follows MobileAction's security practices and policies
+
+### Code review 
+
+- [ ] Pull request title follows [conventional commit format](https://mobileaction.atlassian.net/wiki/spaces/ENG/pages/743112854/Conventional+Commit+Messages) and has a JIRA task id
---
+++ web/src/main/java/co/mobileaction/example/web/service/PostService.java
@@ -0,0 +1,45 @@
+package co.mobileaction.example.web.service;
+
+import co.mobileaction.example.web.model.Post;
+import co.mobileaction.example.web.repository.IPostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author sa
+ * @date 17.05.2021
+ * @time 17:46
+ */
+@Service
+@RequiredArgsConstructor
+public class PostService implements IPostService
+{
+    private final IPostRepository postRepository;
+
+    @Override
+    public void savePost(Post post)
+    {
+        postRepository.save(post);
+    }
+
+    @Override
+    public List<Post> findPosts(Pageable pageable)
+    {
+        return postRepository.findAll(pageable).getContent();
+    }
+
+    @Override
+    public List<Post> findAllPostsOfUser(Long userId)
+    {
+        return postRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public void deletePost(Long postId)
+    {
+        postRepository.deleteById(postId);
+    }
+}
